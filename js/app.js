b604d5d07909234dffecd8bc557bb10f6ab65423
78 mmd 69 <mmdqurani@gmail.com>
2024-04-14 11:05:48 +0330

    Fix : Determining the performance of events

--- js/app.js
+++ js/app.js
@@ -1,21 +1,30 @@
 let $ = document;
 
 let bodyElem = $.body;
-let contextMenu = $.getElementById ("contextMenu");
-let saveValuePageX ;
-let saveValuePageY ;
+let contextMenu = $.getElementById ("contextMenu"); 
 
 bodyElem.addEventListener ("contextmenu" , (event) => {
     event.preventDefault ();
-    saveValuePageX = event.pageX;
-    saveValuePageY = event.pageY;
-    contextMenu.style.left = saveValuePageX + "px";
-    contextMenu.style.top = saveValuePageY + "px";
-    contextMenu.style.display = "flex";
+    if (contextMenu.style.display === "none") {
+        saveValuePageX = event.pageX;
+        saveValuePageY = event.pageY;
+        contextMenu.style.left = event.pageX + "px";
+        contextMenu.style.top = event.pageY + "px";
+        contextMenu.style.display = "flex";
+    }else {
+        contextMenu.style.left = event.pageX + "px";
+        contextMenu.style.top = event.pageY + "px";
+    }
 });
 
 bodyElem.addEventListener ("keydown" , (event) => {
     if (event.key === "Escape") {
+        contextMenu.style.display = "none";
+    }
+});
+
+bodyElem.addEventListener ("click" , () => {
+    if (contextMenu.style.display != "none") {
         contextMenu.style.display = "none";
     }
 });
